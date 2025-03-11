d498da9f9544a7727a4873c24df34fdfee776f42
ballamaissa03 <astoudiouf@esp.sn>
2025-03-11 11:54:09 +0100

    c

--- teste.c
+++ teste.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+int sum(int a, intb){
+    return a + b;
+}
 int main(int argc, char const *argv[])
 {
     printf("Bonjour  \n");
     printf("Bonjourrrrrr c'est Astou bisouss \n");
+
 
     return 0;
 }
