a2d2f065048bc25907dd7ddc5b7ea2484730520b
ballamaissa03 <astoudiouf@esp.sn>
2025-03-18 12:11:27 +0100

    js:mes premiers tp

---
+++ file6.js
@@ -0,0 +1,11 @@
+function Max1(x, y){
+    return x > y ? x : b;
+}
+const Max2 = function(x, y){
+    return x > y ? x : b;
+}
+const Max3 = (x, y) =>{
+    return x > y ? x : b;
+}
+const Max4 = (x, y) => x > y ? x : b;
+let res = Max1(2, 7);
