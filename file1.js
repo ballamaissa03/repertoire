9de5bc8ba2c9786f022705cc84ea87659a70be60
ballamaissa03 <astoudiouf@esp.sn>
2025-03-18 11:46:15 +0100

    Js:mes  premiers tp

---
+++ file1.js
@@ -0,0 +1,11 @@
+console.log("this is js");
+let x = 3;
+let y = 4;
+let somme = x + y;
+console.log(x + " + " + y + " = " + somme);
+console.log(`${x} + ${y} = ${somme}`);
+/*
+Exercice
+
+console.log( " sum( " + x "," + y " = " + somme " ) ");
+*/
---
+++ file2.js
@@ -0,0 +1,10 @@
+console.log("this is js");
+let age = 45;
+let experience = 10;
+if(age > 35)
+    if(experience > 5)
+        console.log("senior");
+    else
+        console.log("junior");
+else
+    console.log("junior");
